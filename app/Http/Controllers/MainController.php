76d6d90488ac86536f53b92f5b51fc332578ef3c
etraeel <iseifollahi@gmail.com>
2023-03-11 15:25:59 +0330

    do5

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Telegram;
+use CustomTelegram;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $telegram = new Telegram( env('TELEGRAM_BOT_TOKEN') , $proxy=['127.0.0.1:8081']);
+        $telegram = new CustomTelegram( env('TELEGRAM_BOT_TOKEN') , $proxy=['127.0.0.1:8081']);
         $chat_id = $telegram->ChatID();
         $message = $telegram->Text();
         $content = array('chat_id' => $chat_id, 'text' => $message);
         $telegram->sendMessage($content);
 
-//        $video= 'https://dl7.digimoviz.top/www2/film/1401/12/Luther.The.Fallen.Sun.2023.480p.WEB-DL.HardSub.DigiMoviez.mp4';
+        $video= 'https://dl7.digimoviz.top/www2/film/1401/12/Luther.The.Fallen.Sun.2023.480p.WEB-DL.HardSub.DigiMoviez.mp4';
 //        $video= '/var/www/html/bakhtakMovie/public/v.mp4';
-//        $content = array('chat_id' => $chat_id, 'video' => $video, 'caption' => 'TEST');
-//        $telegram->sendVideo($content);
-
-        $video= curl_file_create('/var/www/html/bakhtakMovie/public/v.mp4','video/mp4'); // Must be on the same hosting filesystem
-        $content = array('chat_id' => $chat_id, 'video' => $video, 'length'=> '639'); //There was a stange bug with the lenght
+        $content = array('chat_id' => $chat_id, 'video' => $video, 'caption' => 'TEST');
         $telegram->sendVideo($content);
+
+//        $video= curl_file_create('/var/www/html/bakhtakMovie/public/v.mp4','video/mp4'); // Must be on the same hosting filesystem
+//        $content = array('chat_id' => $chat_id, 'video' => $video, 'length'=> '639'); //There was a stange bug with the lenght
+//        $telegram->sendVideo($content);
     }
 }
 
--- app/Http/Helper/CustomTelegram.php
+++ app/Http/Helper/CustomTelegram.php
@@ -2,6 +2,63 @@
 
 class CustomTelegram extends Telegram
 {
+    private $bot_token = '';
+    private $data = [];
+    private $updates = [];
+    private $log_errors;
+    private $proxy;
+    private function sendAPIRequest($url, array $content, $post = true)
+    {
+        if (isset($content['chat_id'])) {
+            $url = $url.'?chat_id='.$content['chat_id'];
+            unset($content['chat_id']);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        }
+        // 		echo "inside curl if";
+        if (!empty($this->proxy)) {
+            // 			echo "inside proxy if";
+            if (array_key_exists('type', $this->proxy)) {
+                curl_setopt($ch, CURLOPT_PROXYTYPE, $this->proxy['type']);
+            }
+
+            if (array_key_exists('auth', $this->proxy)) {
+                curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->proxy['auth']);
+            }
+
+            if (array_key_exists('url', $this->proxy)) {
+                // 				echo "Proxy Url";
+                curl_setopt($ch, CURLOPT_PROXY, $this->proxy['url']);
+            }
+
+            if (array_key_exists('port', $this->proxy)) {
+                // 				echo "Proxy port";
+                curl_setopt($ch, CURLOPT_PROXYPORT, $this->proxy['port']);
+            }
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        if ($result === false) {
+            $result = json_encode(
+                ['ok' => false, 'curl_error_code' => curl_errno($ch), 'curl_error' => curl_error($ch)]
+            );
+        }
+        curl_close($ch);
+        if ($this->log_errors) {
+            if (class_exists('TelegramErrorLogger')) {
+                $loggerArray = ($this->getData() == null) ? [$content] : [$this->getData(), $content];
+                TelegramErrorLogger::log(json_decode($result, true), $loggerArray);
+            }
+        }
+
+        return $result;
+    }
     public function endpoint($api, array $content, $post = true)
     {
         $url = '127.0.0.1:8081/bot'.$this->bot_token.'/'.$api;
